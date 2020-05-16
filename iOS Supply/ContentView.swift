89d780b0c24502a67df6d7a8002533d3819dc20b
Bradley Heenk <Trini8ed@gmail.com>
2020-05-15 20:15:59 -0700

    Initial commit

---
+++ iOS Supply/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Supply
+//
+//  Created by Bradley Heenk on 5/15/20.
+//  Copyright © 2020 Bradley Heenk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
